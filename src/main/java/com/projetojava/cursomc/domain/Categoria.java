8d5e38d303ceb0b1b6df43ed02b31340799bd54e
Yuri Amorim <yuricalixto.uk@gmail.com>
2021-12-21 15:38:22 +0000

    DataBase conectato

--- src/main/java/com/projetojava/cursomc/domain/Categoria.java
+++ src/main/java/com/projetojava/cursomc/domain/Categoria.java
@@ -3,8 +3,18 @@ package com.projetojava.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
 	private String nome;
 	
