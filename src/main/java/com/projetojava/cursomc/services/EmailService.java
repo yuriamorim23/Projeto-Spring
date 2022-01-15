58b7c892db6020c831660fe958c9e7cc961bf9db
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-01-15 19:38:26 +0000

    Email HTML

--- src/main/java/com/projetojava/cursomc/services/EmailService.java
+++ src/main/java/com/projetojava/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.projetojava.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.projetojava.cursomc.domain.Pedido;
@@ -9,4 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	//versao html
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
