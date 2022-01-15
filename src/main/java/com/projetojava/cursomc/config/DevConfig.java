c3357d67fdef88a2ca67a8087d290537843ae467
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-01-15 18:00:04 +0000

    SMTP service

--- src/main/java/com/projetojava/cursomc/config/DevConfig.java
+++ src/main/java/com/projetojava/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.projetojava.cursomc.services.DBService;
+import com.projetojava.cursomc.services.EmailService;
+import com.projetojava.cursomc.services.SmtpEmailService;
 
 // config do profile de test // config do @value para o criar banco quando o padrao estiver true no appicaltion
 @Configuration
@@ -31,4 +33,10 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+	
 }
