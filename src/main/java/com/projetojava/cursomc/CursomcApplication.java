475da88707852d442e8c8f41a8a719ed39b96ebe
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-01-23 17:18:23 +0000

    criacao bucket AWS

--- src/main/java/com/projetojava/cursomc/CursomcApplication.java
+++ src/main/java/com/projetojava/cursomc/CursomcApplication.java
@@ -1,19 +1,27 @@
 package com.projetojava.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.projetojava.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner { // Command permite exucutar um metodo auxiliar
 
+	@Autowired
+	private S3Service s3Service;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);	
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-	}
+		s3Service.uploadFile("C:\\Users\\yurim\\Pictures\\bf.png");
+	}	
 }
 
 
